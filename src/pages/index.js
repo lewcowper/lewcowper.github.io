25bbc138fead4cc333ccd68a1baab717a18e0402
Lew Cowper <lewcowper@gmail.com>
2023-03-22 15:14:23 +1100

    Updated deployment.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='text-blue-500'>
-        Home page
+        Home page test
       </div>
     </>
   )
