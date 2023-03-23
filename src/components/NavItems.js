e77b761e0c7285ef4899b6b302228fd8037eca31
Lew Cowper <lewcowper@gmail.com>
2023-03-23 11:50:36 +1100

    Added page placeholders and links in nav items.

--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -1,9 +1,11 @@
+import Link from 'next/link';
+
 const NavItems = () => {
   return (
-    <ul className="flex items-center">
-      <li>About</li>
-      <li>Projects</li>
-      <li>Interests</li>
+    <ul className="h-10 flex items-center space-x-4 px-4">
+      <li><Link href={'/'}>About</Link></li>
+      <li><Link href={'/projects'}>Projects</Link></li>
+      <li><Link href={'/interests'}>Interests</Link></li>
     </ul>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,16 @@ import { useState, useEffect } from 'react';
 import { MdMenu, MdClose } from 'react-icons/md';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import NavItems from './NavItems';
+import Link from 'next/link';
+
+const mdBreakpoint = 768;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  // Monitor window size to close the menu if the screen exceeds the md breakpoint (768px)
   const checkWindowSize = () => {
-    if (window.innerWidth >= 768) {
+    if (window.innerWidth >= mdBreakpoint) {
       setIsOpen(false);
     }
   };
@@ -23,7 +27,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="w-full h-10 flex justify-between bg-gray-100">
-        <div className="flex items-center space-x-4 p-4">
+        <div className="flex items-center space-x-4 px-4">
           <div className="md:hidden hover:cursor-pointer">
             {isOpen ? (
               <MdClose size={35} onClick={() => setIsOpen(false)} />
@@ -32,18 +36,22 @@ const Navbar = () => {
             )}
           </div>
           <div>
-            <h1>lewcowper</h1>
+            <Link href={'/'}><h1>lewcowper</h1></Link>
           </div>
           <div className="hidden md:flex">
             <NavItems />
           </div>
         </div>
-        <div className="flex items-center space-x-4 p-4">
+        <div className="flex items-center space-x-4 px-4">
           <FaLinkedin size={30} />
           <FaGithub size={30} />
         </div>
       </nav>
-      {isOpen && <div className="flex md:hidden"><NavItems /></div>}
+      {isOpen && (
+        <div className="flex md:hidden">
+          <NavItems />
+        </div>
+      )}
     </>
   );
 };
