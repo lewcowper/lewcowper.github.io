c1fc5f25130ac1c408cd67bdf1aed524fe675be2
Lew Cowper <lewcowper@gmail.com>
2023-03-23 15:08:06 +1100

    Added grids to About page layout.

--- src/components/About.js
+++ src/components/About.js
@@ -4,50 +4,63 @@ import self from '../../public/assets/images/self-s.jpg';
 
 const About = () => {
   return (
-    <div>
-      <p>
-        I'm Lewis Cowper, a software developer based in Sydney, Australia with
-        experience in full-stack web development and operating system
-        implementation.
-      </p>
-      <p>
-        Prior to software development, I accumulated broad experience in project
-        management, commercialisation and engineering roles in the renewable
-        energy and construction industries.
-      </p>
-      <p>Outside of work, I enjoy reading, running and strength training.</p>
-      <div className="image-self">
-        <Image
-          src={self}
-          alt="self"
-          priority={true}
-          width={240}
-          height={240}
-          className="rounded-full"
-        />
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className='order-2 md:order-1'>
+          <p>
+            I'm Lewis Cowper, a software developer based in Sydney, Australia
+            with experience in full-stack web development and operating system
+            implementation.
+          </p>
+          <p>
+            Prior to software development, I accumulated broad experience in
+            project management, commercialisation and engineering roles in the
+            renewable energy and construction industries.
+          </p>
+          <p>
+            Outside of work, I enjoy reading, running and strength training.
+          </p>
+        </div>
+        <div className='order-1 md:order-2'>
+          <Image
+            src={self}
+            alt="self"
+            priority={true}
+            width={320}
+            height={320}
+            className="rounded-full"
+          />
+        </div>
       </div>
-      <div>
-        <h3>Qualifications</h3>
-        <ul>
-          <li>BE (Mechanical), UNSW Sydney</li>
-          <li>MIT (Computer Science), UNSW Sydney</li>
-        </ul>
-        <h3>Languages</h3>
-        <ul>
-          <li>Java</li>
-          <li>JavaScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-        </ul>
-        <h3>Frameworks/Libraries</h3>
-        <ul>
-          <li>Spring Boot</li>
-          <li>React</li>
-          <li>Next.js</li>
-          <li>Tailwind CSS</li>
-        </ul>
+
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div>
+          <h3>Qualifications</h3>
+          <ul>
+            <li>BE (Mechanical), UNSW Sydney</li>
+            <li>MIT (Computer Science), UNSW Sydney</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Languages</h3>
+          <ul>
+            <li>Java</li>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Frameworks/Libraries</h3>
+          <ul>
+            <li>Spring Boot</li>
+            <li>React</li>
+            <li>Next.js</li>
+            <li>Tailwind CSS</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
