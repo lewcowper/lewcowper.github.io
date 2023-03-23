d035d20a5a71b96888015d10371c9c5e0bc41150
Lew Cowper <lewcowper@gmail.com>
2023-03-23 11:27:12 +1100

    Added responsiveness to navbar.

---
+++ src/components/NavItems.js
@@ -0,0 +1,11 @@
+const NavItems = () => {
+  return (
+    <ul className="flex items-center">
+      <li>About</li>
+      <li>Projects</li>
+      <li>Interests</li>
+    </ul>
+  );
+};
+
+export default NavItems;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,50 @@
+import { useState, useEffect } from 'react';
+
 import { MdMenu, MdClose } from 'react-icons/md';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import NavItems from './NavItems';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const checkWindowSize = () => {
+    if (window.innerWidth >= 768) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkWindowSize);
+    return () => {
+      window.removeEventListener('resize', checkWindowSize);
+    };
+  }, []);
+
   return (
-    <nav className="w-full h-10 flex justify-between bg-gray-100">
-      <div className="flex items-center space-x-4 p-2">
-        <div className="md:hidden">
-          <MdMenu size={30} />
+    <>
+      <nav className="w-full h-10 flex justify-between bg-gray-100">
+        <div className="flex items-center space-x-4 p-4">
+          <div className="md:hidden hover:cursor-pointer">
+            {isOpen ? (
+              <MdClose size={35} onClick={() => setIsOpen(false)} />
+            ) : (
+              <MdMenu size={35} onClick={() => setIsOpen(true)} />
+            )}
+          </div>
+          <div>
+            <h1>lewcowper</h1>
+          </div>
+          <div className="hidden md:flex">
+            <NavItems />
+          </div>
         </div>
-        <div>
-          <h1>lewcowper</h1>
+        <div className="flex items-center space-x-4 p-4">
+          <FaLinkedin size={30} />
+          <FaGithub size={30} />
         </div>
-        <ul className="hidden items-center md:flex">
-          <li>About</li>
-          <li>Projects</li>
-          <li>Interests</li>
-        </ul>
-      </div>
-      <div className="flex items-center space-x-4 p-2">
-        <FaLinkedin size={30} />
-        <FaGithub size={30} />
-      </div>
-    </nav>
+      </nav>
+      {isOpen && <div className="flex md:hidden"><NavItems /></div>}
+    </>
   );
 };
 
