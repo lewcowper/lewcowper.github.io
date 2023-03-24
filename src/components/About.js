3207a8600d050ae32e8851bdfe611ce848d60a4e
Lew Cowper <lewcowper@gmail.com>
2023-03-24 17:17:49 +1100

    Minor layout adjustments.

--- src/components/About.js
+++ src/components/About.js
@@ -10,21 +10,21 @@ const About = () => {
     <div className="flex justify-center">
       <div className="grid grid-cols-1 md:grid-cols-5 max-w-5xl">
         <div className="order-2 md:order-1 p-4 md:col-span-3">
-          <p>
+          <p className='text-center sm:text-justify'>
             I'm a software developer based in Sydney, Australia with experience
             in full-stack web development and operating system implementation.
           </p>
-          <p>
-            Prior to software development, I accumulated broad experience in
-            project management, commercialisation and engineering roles in the
-            renewable energy and construction industries.
-          </p>
-          <p>In my spare time, I enjoy reading and running.</p>
           <div className="flex flex-wrap gap-4 justify-evenly p-4">
             {cardData.map((card) => {
               return <Card card={card} />;
             })}
           </div>
+          <p className='text-center sm:text-justify mb-2'>
+            Prior to software development, I accumulated broad experience in
+            project management, commercialisation and engineering roles in the
+            renewable energy and construction industries.
+          </p>
+          <p className='text-center sm:text-justify'>In my spare time, I enjoy reading and running.</p>
         </div>
         <div className="order-1 md:order-2 flex justify-center p-4 md:col-span-2">
           <div>
@@ -32,8 +32,8 @@ const About = () => {
               src={self}
               alt="self"
               priority={true}
-              width={320}
-              height={320}
+              width={270}
+              height={270}
               className="rounded-full"
             />
           </div>
