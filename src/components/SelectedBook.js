c175964e3476fd3ce204c996a214524f0a283153
Lew Cowper <lewcowper@gmail.com>
2023-03-27 12:58:30 +1100

    Added separate link to publisher

--- src/components/SelectedBook.js
+++ src/components/SelectedBook.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { MdOutlineOpenInNew } from 'react-icons/md';
+
 const imagePath = '/assets/images/books/';
 
 const SelectedBook = ({ book }) => {
@@ -10,19 +12,27 @@ const SelectedBook = ({ book }) => {
         {book.subtitle ? book.title + ': ' + book.subtitle : book.title}
       </div>
       <div>{book.author}</div>
-      <div className='w-32 md:w-48'>
-        <Link href={book.link} target="_blank">
-          <Image
-            src={imagePath + book.cover + '.jpg'}
-            alt={book.cover}
-            width={180}
-            height={270}
-            quality={100}
-          />
-        </Link>
+      <div className="w-32 md:w-48">
+        <Image
+          src={imagePath + book.cover + '.jpg'}
+          alt={book.cover}
+          width={180}
+          height={270}
+          quality={100}
+        />
       </div>
       <div>
         <p>{book.note}</p>
+      </div>
+      <div>
+        {'Publisher, image credit: '}
+        <Link
+          href={book.link}
+          target="_blank"
+          className="inline-flex items-center hover:underline"
+        >
+          <div className='pr-1'>{book.publisher}</div> <MdOutlineOpenInNew className="text-md" />
+        </Link>
       </div>
     </div>
   );
