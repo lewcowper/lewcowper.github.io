afbeae0b0df48f59b1dcce7b3477451e92ec4326
Lew Cowper <lewcowper@gmail.com>
2023-03-30 14:48:04 +1100

    Removed grayscale on hover over run image.

--- src/components/Run.js
+++ src/components/Run.js
@@ -4,7 +4,7 @@ const imagePath = '/assets/images/runs/';
 
 const Run = ({ run }) => {
   return (
-    <div className="flex justify-center items-end text-transparent hover:text-white relative hover:grayscale">
+    <div className="flex justify-center items-end text-transparent hover:text-white relative">
       <div className="absolute whitespace-normal text-center w-full text-lg md:text-2xl px-0.5 py-2 font-medium">{run.name}</div>
       <Image
         src={imagePath + run.file + '.jpg'}
