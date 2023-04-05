65dfabb77f8cf8d22ef2f9dfedd47e75fbf361a0
Lew Cowper <lewcowper@gmail.com>
2023-04-05 15:32:00 +1000

    Switched to flex within grid for layout.

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,43 +6,43 @@ const Project = ({ projectData, langIcons, libFraIcons }) => {
   let id = 0;
 
   return (
-    <div className={isShowingMore ? 'row-span-2' : ''}>
-      <div className="py-2 px-4 bg-neutral-100 rounded-lg">
-        <h3>{projectData.title}</h3>
-        <p>{projectData.description}</p>
-        <div>
-          {isShowingMore && (
-            <ul className='pt-2 list-disc pl-4'>
-              {projectData.details.map((detail) => {
-                return <li className="pb-2">{detail}</li>;
-              })}
-            </ul>
-          )}
-        </div>
-        {isShowingMore && <p>Languages: {projectData.languages}.</p>}
+    <div className="py-2 px-4 bg-neutral-100 rounded-lg">
+      <h3>{projectData.title}</h3>
+      <p>{projectData.description}</p>
+      <div>
         {isShowingMore && (
-          <p>Frameworks/Libraries: {projectData.frameworksLibraries}.</p>
+          <ul className="pt-2 list-disc pl-4">
+            {projectData.details.map((detail) => {
+              return <li className="pb-2">{detail}</li>;
+            })}
+          </ul>
         )}
-        <div className="flex justify-between py-2">
-          <div className="flex gap-4">
-            <div className="flex text-2xl sm:text-3xl gap-2">
-              {langIcons.map((icon) => {
-                return <div key={id++}>{icon}</div>;
-              })}
-            </div>
-            <div className="flex text-2xl sm:text-3xl gap-2">
-              {libFraIcons.map((icon) => {
-                return <div key={id++}>{icon}</div>;
-              })}
-            </div>
+      </div>
+      {isShowingMore && projectData.languages.length > 0 && (
+        <p>Languages: {projectData.languages}</p>
+      )}
+      {isShowingMore && projectData.frameworksLibraries.length > 0 && (
+        <p>Frameworks/Libraries: {projectData.frameworksLibraries}</p>
+      )}
+      <div className="flex justify-between py-2">
+        <div className="flex flex-wrap gap-4">
+          <div className="flex text-2xl gap-2">
+            {langIcons.map((icon) => {
+              return <div key={id++}>{icon}</div>;
+            })}
           </div>
-          <button
-            className="hover:underline"
-            onClick={() => setIsShowingMore(!isShowingMore)}
-          >
-            <div>{isShowingMore ? 'Show less' : 'Show more'}</div>
-          </button>
+          <div className="flex text-2xl gap-2">
+            {libFraIcons.map((icon) => {
+              return <div key={id++}>{icon}</div>;
+            })}
+          </div>
         </div>
+        <button
+          className="hover:underline font-medium"
+          onClick={() => setIsShowingMore(!isShowingMore)}
+        >
+          {isShowingMore ? 'Show less' : 'Show more'}
+        </button>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,10 +7,10 @@ import {
   SiReact,
   SiNextdotjs,
   SiSpringboot,
-  SiBootstrap
+  SiBootstrap,
 } from 'react-icons/si';
 import { FaJava } from 'react-icons/fa';
-import {SiC, SiTailwindcss} from 'react-icons/si';
+import { SiC, SiTailwindcss } from 'react-icons/si';
 
 import projectsData from '../../public/assets/data/projects.json';
 
@@ -18,10 +18,38 @@ const Projects = () => {
   return (
     <div className="flex justify-center text-neutral-700 my-4">
       <div className="flex flex-wrap md:flex-nowrap max-w-5xl justify-center w-max">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 lg:mx-0">
-          <Project projectData={projectsData.filter(data => data.title === "Personal Website")[0]} langIcons={[<SiJavascript/>, <SiHtml5 />]} libFraIcons={[<SiNextdotjs />, <SiReact />, <SiTailwindcss />]}/>
-          <Project projectData={projectsData.filter(data => data.title === "Meal Recommendation System")[0]} langIcons={[<SiJavascript/>, <SiHtml5 />, <SiCss3 />]} libFraIcons={[<SiReact />, <SiBootstrap />]}/>
-          <Project projectData={projectsData.filter(data => data.title === "Simple Operating System (SOS)")[0]} langIcons={[<SiC />]} libFraIcons={[]}/>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:mx-0">
+          <div className="flex flex-col gap-4">
+            <Project
+              projectData={
+                projectsData.filter(
+                  (data) => data.title === 'Personal Website'
+                )[0]
+              }
+              langIcons={[<SiJavascript />, <SiHtml5 />]}
+              libFraIcons={[<SiNextdotjs />, <SiReact />, <SiTailwindcss />]}
+            />
+            <Project
+              projectData={
+                projectsData.filter(
+                  (data) => data.title === 'Meal Recommendation System'
+                )[0]
+              }
+              langIcons={[<SiJavascript />, <SiHtml5 />, <SiCss3 />]}
+              libFraIcons={[<SiReact />, <SiBootstrap />]}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <Project
+              projectData={
+                projectsData.filter(
+                  (data) => data.title === 'Simple Operating System (SOS)'
+                )[0]
+              }
+              langIcons={[<SiC />]}
+              libFraIcons={[]}
+            />
+          </div>
         </div>
       </div>
     </div>
