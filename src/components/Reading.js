cbcbdb5c49de66830f7be4c0ba0a7d7bc078482a
Lew Cowper <lewcowper@gmail.com>
2023-04-05 21:09:38 +1000

    Adjusted layout and added overview.

--- src/components/Reading.js
+++ src/components/Reading.js
@@ -1,23 +1,37 @@
 import SelectedBook from './SelectedBook';
 import BookGrid from './BookGrid';
 
-const Reading = ({bookData, selectedBook, setSelectedBook}) => {
+const Reading = ({ bookData, selectedBook, setSelectedBook }) => {
   return (
-    <div className="flex justify-center text-neutral-700">
-      <div className="flex flex-wrap md:flex-nowrap justify-center max-w-5xl">
-        <div className="md:basis-1/2 grow">
-          <SelectedBook book={selectedBook} />
-        </div>
-        <div className="md:basis-1/2 md:max-w-fit">
-          <BookGrid
-            bookData={bookData}
-            selectedBook={selectedBook}
-            setSelectedBook={setSelectedBook}
-          />
+    <>
+      <div className="flex justify-center text-neutral-700">
+        <div className="flex flex-wrap md:flex-nowrap justify-center max-w-5xl">
+          <p className="px-4">
+            I'm avid reader and thoroughly enjoy popular history books about
+            periods of significant social, political and technological change,
+            as well as well-researched biographies about individuals involved in
+            such change. When seeking a lighter read, I enjoy popular science,
+            business books and memoirs by individuals with unique experiences to
+            share. The books on this page are some of my favourites.
+          </p>
         </div>
       </div>
-    </div>
-  )
-}
+      <div className="flex justify-center text-neutral-700 my-4">
+        <div className="flex flex-wrap md:flex-nowrap justify-center max-w-5xl">
+          <div className="md:basis-1/2 grow">
+            <SelectedBook book={selectedBook} />
+          </div>
+          <div className="md:basis-1/2 md:max-w-fit">
+            <BookGrid
+              bookData={bookData}
+              selectedBook={selectedBook}
+              setSelectedBook={setSelectedBook}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Reading
+export default Reading;
