d292e33229be91d2c4d95b8ac7328a875db21dbf
Lew Cowper <lewcowper@gmail.com>
2023-04-06 09:09:19 +1000

    Initial text.

--- src/components/Running.js
+++ src/components/Running.js
@@ -25,15 +25,30 @@ const Running = () => {
   }, []);
 
   return (
-    <div className="flex justify-center text-neutral-700">
-      <div className="flex flex-wrap md:flex-nowrap justify-center max-w-5xl">
-        <div className="grid grid-cols-3 md:grid-cols-4 grid-rows-3 md:grid-rows-2 m-4">
-          {runsArray.map((run) => {
-            return <Run run={run} />;
-          })}
+    <>
+      <div className="flex justify-center text-neutral-700">
+        <div className="flex flex-wrap md:flex-nowrap justify-center max-w-5xl">
+          <p className="px-4">
+            In my youth, I enjoyed competing in track & field athletics and
+            playing Australia rules football. While I no longer play these
+            sports, I still love running. Road running is my favourite form of
+            exercise and I enjoy training to improve my 5K and 10K times. When I
+            can, I enjoy running along parts of the Bondi to Coogee coastal
+            track in Sydney's Eastern Suburbs. The photos on this page show some
+            of the scenic parts of this track and their locations.
+          </p>
         </div>
       </div>
-    </div>
+      <div className="flex justify-center text-neutral-700">
+        <div className="flex flex-wrap md:flex-nowrap justify-center max-w-5xl">
+          <div className="grid grid-cols-3 md:grid-cols-4 grid-rows-3 md:grid-rows-2 m-4">
+            {runsArray.map((run) => {
+              return <Run run={run} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
